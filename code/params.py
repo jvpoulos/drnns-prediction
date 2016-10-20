3fc4178dcb6183d0f553ad120e9656eb2f9f2b5e
jvpoulos <jvpoulos@gmail.com>
2016-10-20 16:38:10 -0400

    fix params

--- code/params.py
+++ code/params.py
@@ -4,8 +4,8 @@ import numpy as np
 
 # Define parameters for cross-validation
 params_dict = {}
-params_dict['batch_sizes'] = (23,46,161)
-params_dict['dropouts'] = (0.25, 0.5)
+params_dict['batch_sizes'] = (7, 14)
+params_dict['dropouts'] = (0.25, 0.5, 0.75)
 params_dict['hidden_nodes'] = (32,64,128)
 params_dict['activations'] = ('softmax', 'tanh', 'sigmoid')
 params_dict['inits'] = ('glorot_uniform', 'glorot_normal', 'uniform')
