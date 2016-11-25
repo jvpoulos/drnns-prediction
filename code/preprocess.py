527db0f918429ac69dc32366110e9f2c7c1a382b
jvpoulos <jvpoulos@gmail.com>
2016-11-24 23:23:23 -0800

    tfidf fit after split

--- code/preprocess.py
+++ code/preprocess.py
@@ -7,34 +7,46 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import pandas as pd
 import numpy as np
 from datetime import date
+import nltk
 import cPickle as pkl
 
-def set_trace():
-    from IPython.core.debugger import Pdb
-    import sys
-    Pdb(color_scheme='Linux').set_trace(sys._getframe().f_back)
-
 # Read data
+print('Reading data')
+
 data = pd.read_csv("data/stocknews/Combined_News_DJIA.csv")
 
 # Concatenate all news into one
 data["combined_news"] = data.filter(regex=("Top.*")).apply(lambda x: ''.join(str(x.values)), axis=1)
 
-# Convert to feature vector
-feature_extraction = TfidfVectorizer() # converts all characters to lowercase by default
-X = feature_extraction.fit_transform(data["combined_news"].values)
-
 # Split into training, validation, and test set
+print('Split data')
+
 training_end = date(2014,12,31)
 num_training = len(data[pd.to_datetime(data["Date"]) <= training_end])
 
-X_train = X[:num_training]
-X_test = X[num_training:]
+X_train_raw = data["combined_news"].values[:num_training]
+X_test_raw = data["combined_news"].values[num_training:]
 
 y_train = data["Label"].values[:num_training]
 y_test = data["Label"].values[num_training:]
 
+# Feature extraction
+print('Extract feature vector')
+
+# Fit tf-idf 
+nltk.download(['stopwords', 'wordnet']) # get stopwords from nltk
+stopwords = nltk.corpus.stopwords.words('english')
+
+feature_extraction = TfidfVectorizer(stop_words=stopwords,lowercase=True) # remove stopwords and converts characters to lowercase during preprocessing step
+X_train = feature_extraction.fit_transform(X_train_raw) # learn vocabulary and idf from training features
+X_test = feature_extraction.transform(X_test_raw) # apply training fit to test data
+
+print('X_train shape:', X_train.shape)
+print('X_test shape:', X_test.shape)
+
 # Save train and test sets to disk
+print('Save to disk')
+
 pkl.dump(X_train, open('data/X_train.np', 'wb'))
 pkl.dump(X_test, open('data/X_test.np', 'wb'))
 
