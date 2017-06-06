e476ab1f71cb0fbdd530dd1c1f1cfa2e035fb8e6
jvpoulos <jvpoulos@gmail.com>
2017-06-06 15:45:36 -0400

    update readme and version changes

--- code/final_predict.py
+++ code/final_predict.py
@@ -10,7 +10,7 @@ from scipy.sparse import csr_matrix
 from sklearn.metrics import roc_curve, auc, roc_auc_score
 
 from keras import backend as K
-from keras.utils.visualize_util import plot, model_to_dot
+from keras.utils.vis_utils import plot_model, model_to_dot
 from keras.models import Sequential
 from keras.layers import GRU, Dense, Masking, Dropout, Activation
 from keras.callbacks import Callback, EarlyStopping, ModelCheckpoint
@@ -100,7 +100,7 @@ model.add(Dense(output_dim, activation=activation))
 
 # Visualize model
 
-plot(model, to_file='results/final_model.png', # Plot graph of model
+plot_model(model, to_file='results/final_model.png', # Plot graph of model
   show_shapes = True,
   show_layer_names = False)
 
