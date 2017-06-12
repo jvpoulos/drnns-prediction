b5fb8603f11fe05035be4fe1ebc69aecfa54cc8b
jvpoulos <jvpoulos@gmail.com>
2017-06-12 16:05:19 -0400

    training and test code for patents

--- code/final_predict_patents.py
+++ code/final_predict_patents.py
@@ -1,4 +1,4 @@
-## Trains best final model and saves weights at each epoch
+## Loads best checkpointed model and makes prediciton on test set
 
 from __future__ import print_function
 import sys
@@ -8,16 +8,17 @@ from itertools import product
 import cPickle as pkl
 
 from keras import backend as K
+from keras.utils.vis_utils import plot_model, model_to_dot
 from keras.models import Sequential
 from keras.layers import GRU, Dense, Masking, Dropout, Activation
-from keras.callbacks import Callback,EarlyStopping, ModelCheckpoint,CSVLogger
+from keras.callbacks import Callback, EarlyStopping, ModelCheckpoint
 from keras.optimizers import RMSprop
 
 from utils import set_trace, plot_ROC
 
 # Load saved data
 
-print('Load saved data')
+print('Load saved test data')
 
 X_train = pkl.load(open('data/X_train_patents.np', 'rb'))
 X_val = pkl.load(open('data/X_val_patents.np', 'rb'))
@@ -28,13 +29,12 @@ y_val = pkl.load(open('data/y_val_sales.np', 'rb')) # sales
 
 # Define network structure
 
-epochs = int(sys.argv[-1])
-nb_timesteps = 1
+nb_timesteps = 1 
 nb_classes = 2
-nb_features = X_train.shape[1]
+nb_features = X_test.shape[1]
 output_dim = 1
 
-# Define model parameters
+# Define cross-validated model parameters
 
 batch_size = 8
 dropout = 0.5
@@ -51,8 +51,7 @@ X_train = X_train[2:X_train.shape[0]] # drop first 2 samples so batch size is di
 X_val= np.resize(X_val, (X_val.shape[0], nb_timesteps, X_val.shape[1]))
 X_val = X_val[1:X_val.shape[0]] # drop first sample so batch size is divisible 
 
-print('X_train shape:', X_train.shape)
-print('X_val shape:', X_val.shape)
+X_test= np.resize(X_test, (X_test.shape[0], nb_timesteps, X_test.shape[1]))
 
 # Reshape y to two dimensions
 # Should have shape (batch_size, output_dim)
@@ -63,8 +62,7 @@ y_train = y_train[2:y_train.shape[0]] # drop first 2 samples so batch size is di
 y_val = np.resize(y_val, (y_val.shape[0], output_dim))
 y_val = y_val[1:y_val.shape[0]] # drop first sample so batch size is divisible 
 
-print('y_train shape:', y_train.shape)
-print('y_val shape:', y_train.shape)
+y_test = np.resize(y_test, (y_test.shape[0], output_dim))
 
 # Initiate sequential model
 
@@ -104,35 +102,43 @@ model.add(GRU(nb_hidden, stateful=True, init=initialization))
 model.add(Dropout(dropout)) 
 model.add(Dense(output_dim, activation=activation))
 
+# Visualize model
+
+plot_model(model, to_file='results/final_model.png', # Plot graph of model
+  show_shapes = True,
+  show_layer_names = False)
+
+model_to_dot(model,show_shapes=True,show_layer_names = False).write('results/final_model.dot', format='raw', prog='dot') # write to dot file
+
+# Load weights
+model.load_weights(sys.argv[-1])
+
 # Configure learning process
 
 model.compile(optimizer='rmsprop',
               loss='mean_absolute_error',
               metrics=['mean_absolute_error'])
 
-# Prepare model checkpoints and callbacks
+print("Created model and loaded weights from file")
 
-filepath="results/weights/weights-{val_mean_absolute_error:.5f}.hdf5"
-checkpointer = ModelCheckpoint(filepath=filepath, verbose=0, save_best_only=True)
+# Evaluation 
 
-class LearningRateTracker(Callback):
-    def on_epoch_end(self, epoch, logs={}):
-        optimizer = self.model.optimizer
-        lr = K.eval(optimizer.lr * (1. / (1. + optimizer.decay * optimizer.iterations)))
-        print('\nLR: {:.6f}\n'.format(lr))
+print('Generate predictions on test data')
 
-# Training 
+y_pred_test = model.predict(X_test, batch_size=batch_size, verbose=1) # generate output predictions for test samples, batch-by-batch
 
-print('Training')
-for i in range(epochs):
-    csv_logger = CSVLogger('results/training_log.csv', separator=',', append=True)
-    print('Epoch', i+1, '/', epochs)
-    model.fit(X_train,
-              y_train,
-              batch_size=batch_size,
-              verbose=1,
-              nb_epoch=1,
-              shuffle=False, # turn off shuffle to ensure training data patterns remain sequential
-              callbacks=[checkpointer,csv_logger,LearningRateTracker()],
-              validation_data=(X_val, y_val))
-    model.reset_states()
+np.savetxt("results/ok-pred/sales-test-pred.csv", y_pred_test, delimiter=",")
+
+# Get fits on training and validation set for plots
+
+print('Generate predictions on training set')
+
+y_pred_train = model.predict(X_train, batch_size=batch_size, verbose=1) 
+
+np.savetxt("results/ok-pred/sales-train-pred.csv", y_pred_train, delimiter=",")
+
+print('Generate predictions on validation set')
+
+y_pred_val = model.predict(X_val, batch_size=batch_size, verbose=1) 
+
+np.savetxt("results/ok-pred/sales-val-pred.csv", y_pred_val, delimiter=",")
