ef128033ebbd5fea091756b5ceb1e3510482c5f0
jvpoulos <jvpoulos@gmail.com>
2017-06-20 13:34:39 -0400

    debug

--- code/final_predict_ok.py
+++ code/final_predict_ok.py
@@ -20,7 +20,7 @@ os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
 os.environ["CUDA_VISIBLE_DEVICES"]= "{}".format(gpu)
 
 from tensorflow.python.client import device_lib
-print device_lib.list_local_devices()
+print(device_lib.list_local_devices())
 
 # Load saved data
 
