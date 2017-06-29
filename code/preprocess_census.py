47006dc8dfdaacce1828dd06e7355acec7e0c6be
jvpoulos <jvpoulos@gmail.com>
2017-06-29 10:40:33 -0400

    debug

--- code/preprocess_census.py
+++ code/preprocess_census.py
@@ -18,16 +18,16 @@ tenancy_y_train = pd.read_csv("data/census/tenancy-y-train.csv")
 tenancy_y_test = pd.read_csv("data/census/tenancy-y-test.csv") 
 
 gini_x_train_placebo = pd.read_csv("data/census/gini-x-train-placebo.csv") 
-gini_x_val_placebo = pd.read_csv("data/census/gini-x-val-placebo.csv") 
+gini_x_test_placebo = pd.read_csv("data/census/gini-x-test-placebo.csv") 
 
 gini_y_train_placebo = pd.read_csv("data/census/gini-y-train-placebo.csv") 
-gini_y_val_placebo = pd.read_csv("data/census/gini-y-val-placebo.csv") 
+gini_y_test_placebo = pd.read_csv("data/census/gini-y-test-placebo.csv") 
 
 tenancy_x_train_placebo = pd.read_csv("data/census/tenancy-x-train-placebo.csv") 
-tenancy_x_val_placebo = pd.read_csv("data/census/tenancy-x-val-placebo.csv") 
+tenancy_x_test_placebo = pd.read_csv("data/census/tenancy-x-test-placebo.csv") 
 
 tenancy_y_train_placebo = pd.read_csv("data/census/tenancy-y-train-placebo.csv") 
-tenancy_y_val_placebo = pd.read_csv("data/census/tenancy-y-val-placebo.csv") 
+tenancy_y_test_placebo = pd.read_csv("data/census/tenancy-y-test-placebo.csv")
 
 
 # Save train and test sets to disk
@@ -46,13 +46,13 @@ pkl.dump(tenancy_y_train, open('data/tenancy_y_train.np', 'wb'))
 pkl.dump(tenancy_y_test, open('data/tenancy_y_test.np', 'wb'))
 
 pkl.dump(gini_x_train_placebo, open('data/gini_x_train_placebo.np', 'wb'))
-pkl.dump(gini_x_val_placebo, open('data/gini_x_val_placebo.np', 'wb'))
+pkl.dump(gini_x_test_placebo, open('data/gini_x_test_placebo.np', 'wb'))
 
 pkl.dump(tenancy_x_train_placebo, open('data/tenancy_x_train_placebo.np', 'wb'))
-pkl.dump(tenancy_x_val_placebo, open('data/tenancy_x_val_placebo.np', 'wb'))
+pkl.dump(tenancy_x_test_placebo, open('data/tenancy_x_test_placebo.np', 'wb'))
 
 pkl.dump(gini_y_train_placebo, open('data/gini_y_train_placebo.np', 'wb'))
-pkl.dump(gini_y_val_placebo, open('data/gini_y_val_placebo.np', 'wb'))
+pkl.dump(gini_y_test_placebo, open('data/gini_y_test_placebo.np', 'wb'))
 
 pkl.dump(tenancy_y_train_placebo, open('data/tenancy_y_train_placebo.np', 'wb'))
-pkl.dump(tenancy_y_val_placebo, open('data/tenancy_y_val_placebo.np', 'wb'))
+pkl.dump(tenancy_y_test_placebo, open('data/tenancy_y_test_placebo.np', 'wb'))
