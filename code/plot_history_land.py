ba7814cf17cf6a69bc7d040e4d18702b131f9f6b
jvpoulos <jvpoulos@gmail.com>
2017-08-05 10:11:15 -0400

    new plot script

---
+++ code/plot_history_land.py
@@ -0,0 +1,17 @@
+import sys 
+import matplotlib.pyplot as plt
+import numpy as np
+from utils import set_trace
+
+dataname = sys.argv[-1]
+
+# Read training log
+history = np.genfromtxt(sys.argv[-2], names=True, delimiter=",")
+
+# Summarize history for MAE
+plt.plot(history['mean_absolute_error'])
+plt.title(dataname)
+plt.ylabel('Mean absolute error')
+plt.xlabel('Epoch')
+plt.legend(['Training set'], loc='upper left')
+plt.savefig('results/land-plots/mae-{}'.format(dataname))
