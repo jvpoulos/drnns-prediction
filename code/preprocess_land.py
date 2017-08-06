5178f595e1c18504169ea71d6572b531fae38b95
jvpoulos <jvpoulos@gmail.com>
2017-08-05 20:10:54 -0400

    debug

--- code/preprocess_land.py
+++ code/preprocess_land.py
@@ -8,29 +8,29 @@ folder= sys.argv[-1] # 'analysis-12' or 'analysis-34'
 # Read data
 print('Reading data in data/{}'.format(folder))
 
-homesteads_x_train_treated = pd.read_csv("data/{}/patents-public/treated/homesteads-x-train.csv".format(folder)) # Treated
-homesteads_x_test_treated = pd.read_csv("data/{}/patents-public/treated/homesteads-x-test.csv".format(folder)) 
+homesteads_x_train_treated = pd.read_csv("data/patents-public/{}/treated/homesteads-x-train.csv".format(folder)) # Treated
+homesteads_x_test_treated = pd.read_csv("data/patents-public/{}/treated/homesteads-x-test.csv".format(folder)) 
 
-homesteads_y_train_treated = pd.read_csv("data/{}/patents-public/treated/homesteads-y-train.csv".format(folder)) 
-homesteads_y_test_treated = pd.read_csv("data/{}/patents-public/treated/homesteads-y-test.csv".format(folder)) 
+homesteads_y_train_treated = pd.read_csv("data/patents-public/{}/treated/homesteads-y-train.csv".format(folder)) 
+homesteads_y_test_treated = pd.read_csv("data/patents-public/{}/treated/homesteads-y-test.csv".format(folder)) 
 
-sales_x_train_treated = pd.read_csv("data/{}/patents-public/treated/sales-x-train.csv".format(folder)) 
-sales_x_test_treated = pd.read_csv("data/{}/patents-public/treated/sales-x-test.csv".format(folder)) 
+sales_x_train_treated = pd.read_csv("data/patents-public/{}/treated/sales-x-train.csv".format(folder)) 
+sales_x_test_treated = pd.read_csv("data/patents-public/{}/treated/sales-x-test.csv".format(folder)) 
 
-sales_y_train_treated = pd.read_csv("data/{}/patents-public/treated/sales-y-train.csv".format(folder)) 
-sales_y_test_treated = pd.read_csv("data/{}/patents-public/treated/sales-y-test.csv".format(folder)) 
+sales_y_train_treated = pd.read_csv("data/patents-public/{}/treated/sales-y-train.csv".format(folder)) 
+sales_y_test_treated = pd.read_csv("data/patents-public/{}/treated/sales-y-test.csv".format(folder)) 
 
-homesteads_x_train_control = pd.read_csv("data/{}/patents-public/control/homesteads-x-train.csv".format(folder)) # Control
-homesteads_x_test_control = pd.read_csv("data/{}/patents-public/control/homesteads-x-test.csv".format(folder)) 
+homesteads_x_train_control = pd.read_csv("data/patents-public/{}/control/homesteads-x-train.csv".format(folder)) # Control
+homesteads_x_test_control = pd.read_csv("data/patents-public/{}/control/homesteads-x-test.csv".format(folder)) 
 
-homesteads_y_train_control = pd.read_csv("data/{}/patents-public/control/homesteads-y-train.csv".format(folder)) 
-homesteads_y_test_control = pd.read_csv("data/{}/patents-public/control/homesteads-y-test.csv".format(folder)) 
+homesteads_y_train_control = pd.read_csv("data/patents-public/{}/control/homesteads-y-train.csv".format(folder)) 
+homesteads_y_test_control = pd.read_csv("data/patents-public/{}/control/homesteads-y-test.csv".format(folder)) 
 
-sales_x_train_control = pd.read_csv("data/{}/patents-public/control/sales-x-train.csv".format(folder)) 
-sales_x_test_control = pd.read_csv("data/{}/patents-public/control/sales-x-test.csv".format(folder)) 
+sales_x_train_control = pd.read_csv("data/patents-public/{}/control/sales-x-train.csv".format(folder)) 
+sales_x_test_control = pd.read_csv("data/patents-public/{}/control/sales-x-test.csv".format(folder)) 
 
-sales_y_train_control = pd.read_csv("data/{}/patents-public/control/sales-y-train.csv".format(folder)) 
-sales_y_test_control = pd.read_csv("data/{}/patents-public/control/sales-y-test.csv".format(folder)) 
+sales_y_train_control = pd.read_csv("data/patents-public/{}/control/sales-y-train.csv".format(folder)) 
+sales_y_test_control = pd.read_csv("data/patents-public/{}/control/sales-y-test.csv".format(folder)) 
 
 
 # Save train and test sets to disk
