d208533a8c2e505d4b0d570bb763e32770734153
jvpoulos <jvpoulos@gmail.com>
2017-08-06 07:55:48 -0400

    debug

--- code/plot_history_land.py
+++ code/plot_history_land.py
@@ -3,16 +3,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 from utils import set_trace
 
-dataname = sys.argv[-2]
+dataname= sys.argv[-3]
+
+title = sys.argv[-2]
 
 analysis= sys.argv[-1]
 
 # Read training log
-history = np.genfromtxt(sys.argv[-2], names=True, delimiter=",")
+history = np.genfromtxt(datname, names=True, delimiter=",")
 
 # Summarize history for MAE
 plt.plot(history['mean_absolute_error'])
-plt.title(dataname)
+plt.title(title)
 plt.ylabel('Mean absolute error')
 plt.xlabel('Epoch')
 plt.legend(['Training set'], loc='upper left')
