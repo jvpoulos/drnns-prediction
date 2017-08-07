f11dfd74314af6286450ac7855f81f4271aa010f
jvpoulos <jvpoulos@gmail.com>
2017-08-07 06:18:54 -0400

    rm penalty and inc batch size

--- code/train_land.py
+++ code/train_land.py
@@ -42,8 +42,8 @@ output_dim = 1
 # Define model parameters
 
 dropout = 0.5
-penalty = 0.001
-batch_size = 12
+penalty = 0
+batch_size = 64
 nb_hidden = 256
 activation = 'linear'
 initialization = 'glorot_normal'
